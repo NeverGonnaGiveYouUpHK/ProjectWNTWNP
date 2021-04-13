06c13cbccc220404ec19f62d65a5378c9c46dc8d
Viliam Geffert <60324080+w1lo@users.noreply.github.com>
2021-04-13 15:15:41 +0200

    Created ServerManager class and moved PermissionManager to a class

---
+++ api/permission.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+
+
+//Permission command callback.
+//
+//  Args: msg - Discord Message Object
+//
+//  Return: Embeded Message
+module.exports = function(msg) {
+
+    
+
+    
+    
+}
---
+++ server/serverManager.js
@@ -0,0 +1,22 @@
+
+
+module.exports = class ServerManager {
+
+	constructor(serverID) {
+		
+
+		this.serverID = serverID;
+		// In future: expand
+
+	}
+
+	setConfiguration(configObject) {
+
+	}
+
+
+	Permissions = require('./permissionManager.js');
+
+	#serverConfigObj;
+	
+}
