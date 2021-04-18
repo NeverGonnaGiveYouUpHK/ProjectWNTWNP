61550a3bb7fb41ef1b4a585831522b677bd5f2fb
GLise710 <matejkozub124@gmail.com>
2021-04-18 20:31:45 +0200

    New entry point code

--- wntwnp.js
+++ wntwnp.js
@@ -2,14 +2,22 @@ require('dotenv').config();
 
 const Discord = require('discord.js');
 
+const config = require('./config/config');
+
 global.bot = new Discord.Client();
 global.bot.login(process.env.TOKEN);
 
-const prefix = "!";
-
 global.users = new Map();
 
+const users = global.users;
+const bot = global.bot;
+
+
+const prefix = "!";
+
 const reminderHandle = require('./handles/reminderHandle');
+const timezoneHandle = require('./handles/timezoneHandle');
+
 const help = require('./help');
 
 bot.on('ready', () => {
@@ -23,9 +31,27 @@ bot.on('ready', () => {
 		
 		const args = split.slice(1, split.length);
 
+
+
+		const userID = msg.author.id;
+		let user;
+		if (!users.has(userID)){
+			user = new config.User(userID);
+			await user.initialize();
+			users.set(userID, user);
+		} else {
+			user = users.get(userID);
+		}
+
+
+
 		switch (command){
 			case 'reminder':
-				reminderHandle(msg, args);
+				reminderHandle(msg, user, args);
+				break;
+
+			case 'timezone':
+				timezoneHandle(msg, user, args);
 				break;
 
 			case 'help':
