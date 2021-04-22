75312d121611d7b49aef1f6fead705f0b0fbb9eb
Richard Gerboc <58309743+GerbyRich@users.noreply.github.com>
2021-04-22 19:56:30 +0200

    Update help.js

--- help.js
+++ help.js
@@ -1,12 +1,22 @@
 const discord = require("discord.js");
 
-module.exports = function(){
+module.exports = function(args){
     var holder = new discord.MessageEmbed()  
     .setTitle("WNTWNP Bot command list: ")
     .setFooter("WNTWNP Bot by Never Gonna Give You Up")
-    .setColor("#fcac34")
+    .setColor("#fcac34");
 
-    .addField("!help", "Display help.")
+    switch (args[0]){
+        case undefined:
+            holder.addField("!help", "Display help.")
+            .addField("!permission", "Display and set permissions.")
+            .addField("!task", "Create and display tasks.")
+            .addField("!team", "Create and manage teams.")
+            .addField("!reminder", "Set and manage a reminders.")
+            .addField("!timezone", "Set your timezone.")
+            .addField("!dateformat", "Customize your date format.")
+            break;
+    }
 
     return holder;
 }
